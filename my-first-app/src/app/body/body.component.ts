3dedd24163886f82b693882e9209738b16aebd9c
neelisetty.5655 <vneelisetty@raytex.co.in>
2022-02-24 22:05:57 +0530

    day 6

---
+++ my-first-app/src/app/body/body.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  img1 =".././assets/images/image15.png"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+num=4;
+link="/assets/images/image15.png"
+data={a:1,b:2};
+
+}
+
